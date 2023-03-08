a3778bfb9e1789623153ae9c7366a3ff835deefd
Mateo VL <127341127+mv318@users.noreply.github.com>
2023-03-08 13:47:45 -0600

    Add files via upload

---
+++ EaterQuiz/EaterQuizUITests/EaterQuizUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EaterQuizUITestsLaunchTests.swift
+//  EaterQuizUITests
+//
+//  Created by Villafuerte Langford, Mateo on 3/8/23.
+//
+
+import XCTest
+
+final class EaterQuizUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
